effc4e2da91b8fe5b9306b0bbe2b4be6a8279ef6
viniciomoralesec <viniciomorales.uio@gmail.com>
2024-08-01 18:50:55 -0500

    Tarea 1

--- tarea.php
+++ tarea.php
@@ -1,19 +1,32 @@
 
 <?php
 
-$Item       = 'Notebook';
+$Item       = 'Notebook ';
+$Model      = 'Dell 3140';
 $TransDate  = 01/01/2024;
 $Qty        = 2;
 $Amount     = 990.808080;
 $Posting    = true;
+$Puntaje    = 5;
 $Arreglo    = array();
 $Arreglo    = [1,2,3,4,5,6,7,8,9,10];
+
+echo ($Qty * $Amount);
+
+echo($Item . $Model);
+
+if($Puntaje >7)
+echo('Aprobado');
+else
+echo('Reprobado');
+
 
 while ($i < count($Arreglo))
 {
     echo '<div class="card-opening"><b>'.$Arreglo[$i]. '</b></div>';
     $i++;
 }
+
 ?>
 
 
