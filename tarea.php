59a398bf9ae332da00a85fe868a35ac4f709e3ed
viniciomoralesec <viniciomorales.uio@gmail.com>
2024-08-01 18:43:01 -0500

    Tarea1

--- tarea.php
+++ tarea.php
@@ -1,14 +1,19 @@
 
 <?php
-//phpinfo();
 
 $Item       = 'Notebook';
 $TransDate  = 01/01/2024;
 $Qty        = 2;
-$Amount     = 990.80;
+$Amount     = 990.808080;
 $Posting    = true;
+$Arreglo    = array();
+$Arreglo    = [1,2,3,4,5,6,7,8,9,10];
 
-echo($Qty*$Amount);
+while ($i < count($Arreglo))
+{
+    echo '<div class="card-opening"><b>'.$Arreglo[$i]. '</b></div>';
+    $i++;
+}
 ?>
 
 
